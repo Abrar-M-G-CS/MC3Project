00dcb13b05139f307f472bd964300960fd83ed99
mnerahawn <mnerahawncs@gmail.com>
2024-01-31 10:45:57 +0300

    mnerah change

---
+++ MC3Project/Views/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  MC3Project
+//
+//  Created by mnerah awn on 19/07/1445 AH.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
