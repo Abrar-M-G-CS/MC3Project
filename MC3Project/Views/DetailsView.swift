98694b8f4456ab4e46ef290518fffb62bf9d667f
mnerahawn <mnerahawncs@gmail.com>
2024-01-31 11:56:44 +0300

    last change

--- MC3Project/Views/DetailsView.swift
+++ MC3Project/Views/DetailsView.swift
@@ -1,21 +1,24 @@
 import SwiftUI
 
 struct DetailsView: View {
+    // State variables to track rating, selected tab, and like status
     @State private var currentRating = 4
     @State private var selectedTab: Tab = .overview
     @State private var isLiked = false
+
+    // Enumeration for different tabs
     enum Tab {
         case overview
         case comments
         case none
     }
+
     var body: some View {
-        // ScrollView allows scrolling through the content
         ScrollView {
             VStack(alignment: .trailing) {
-                // ZStack overlays multiple views
+                // ZStack for overlaying the cafe image and like button
                 ZStack {
-                    // Image representing the cafe
+                    // Cafe image
                     Image("cafe1")
                         .resizable()
                         .scaledToFit()
@@ -23,9 +26,10 @@ struct DetailsView: View {
                         .edgesIgnoringSafeArea(.all)
                         .cornerRadius(18)
                         .padding(.top, -80)
-                    
-                    // Like button
+
+                    // Like button with heart icon
                     Button(action: {
+                        // Toggle the like status
                         isLiked.toggle()
                     }) {
                         Image(systemName: isLiked ? "heart.fill" : "heart")
@@ -35,13 +39,14 @@ struct DetailsView: View {
                             .padding(20)
                             .background(Circle().fill(Color.white).opacity(0.9))
                     }
-                    .padding(.top, 230).padding(.trailing, 300)
+                    .padding(.top, 230)
+                    .padding(.trailing, 300)
                 }
-                
-                // HStack for star ratings
+
+                // Star ratings and cafe information
                 HStack(alignment: .top) {
                     HStack {
-                        // Display star icons based on the current rating
+                        // Display star ratings using a loop
                         ForEach(0..<5) { index in
                             Image(systemName: "star.fill")
                                 .foregroundColor(index < currentRating ? .yellow : .gray)
@@ -49,75 +54,120 @@ struct DetailsView: View {
                                 .frame(width: 20, height: 20)
                                 .padding(.leading, -10)
                         }
-                    }.padding(.trailing, 130)
-                    
-                    // Information about the cafe
+                    }
+                    .padding(.trailing, 130)
+
+                    // Cafe name and pricing information
                     VStack(alignment: .trailing) {
                         Text("نمق كافيه |Namg")
                             .font(.headline)
-                        
+
                         HStack(alignment: .top) {
                             Text("٢٠-٤٠ رس")
-                                .font(Font.custom("SF Arabic", size: 12).weight(.bold))
+                                .font(
+                                    Font.custom("SF Arabic", size: 12)
+                                        .weight(.bold)
+                                )
                                 .multilineTextAlignment(.center)
                                 .foregroundColor(Color(red: 0.64, green: 0.63, blue: 0.63))
                                 .frame(width: 63, height: 19, alignment: .top)
-                            
+
+                            // Display "rang money" image
                             Image("rang money")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .frame(width: 24, height: 23)
                                 .clipped()
+                                .padding(.leading, -10)
                         }
                     }
                 }
                 .padding()
-                
+
                 // Divider line
-                Divider().background(Color.gray)
-                
+                Divider()
+                    .background(Color.gray)
+
                 // Tabs for Comments and Overview
                 HStack {
                     Text("التعليقات")
-                        .font(Font.custom("SF Arabic", size: 18))
+                        .font(Font.custom("SF Arabic", size: 20))
                         .multilineTextAlignment(.center)
                         .foregroundColor(selectedTab == .comments ? .black.opacity(0.36) : .gray)
                         .frame(width: 82, height: 27, alignment: .top)
                         .onTapGesture {
+                            // Switch to the Comments tab
                             selectedTab = .comments
                         }
-                    
+
                     Text("نظرة عامة")
                         .font(
-                            Font.custom("SF Arabic", size: 18)
+                            Font.custom("SF Arabic", size: 20)
                                 .weight(.semibold)
                         )
                         .multilineTextAlignment(.center)
                         .foregroundColor(selectedTab == .overview ? Color(red: 0.41, green: 0.16, blue: 0.38) : .gray)
                         .frame(width: 82, height: 27, alignment: .top)
                         .onTapGesture {
+                            // Switch to the Overview tab
                             selectedTab = .overview
                         }
                 }
                 .padding()
-                
-                // Display content based on the selected tab
+
+                // Content based on the selected tab
                 if selectedTab == .overview {
+                    // Display icons and information for the Overview tab
+                    HStack(alignment: .top) {
+                        HStack {
+                            Image("Music")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 10, height: 10)
+                                .padding(.trailing, 0)
+                            Image("disabilities")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 15, height: 15)
+                                .padding(.trailing, 150)
+                        }
+                        Text("وقت الذروة:  ٧-٩ م")
+                            .font(
+                                Font.custom("SF Arabic", size: 16)
+                                    .weight(.semibold)
+                            )
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(red: 0.41, green: 0.16, blue: 0.38))
+                            .padding(.leading, 20)
+
+                        // Display "Peak time" image
+                        Image("Peak time")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 24, height: 23)
+                            .clipped()
+                            .padding(.trailing, 20)
+                    }
+
+                    // Detailed overview of the cafe
                     Text("وجهة واسعة وبسيطة تقدَّم فيها القهوة الساخنة والكعك والآيس كريم.\nيتوفر:\nمواقف مجانية.\nشبكة انترنت مجانية.")
-                        .font(Font.custom("Inter", size: 12))
+                        .font(Font.custom("Inter", size: 18))
                         .multilineTextAlignment(.trailing)
                         .foregroundColor(.black)
                         .frame(width: 349, height: 136, alignment: .topTrailing)
                         .padding(.trailing, 20)
                 } else if selectedTab == .comments {
                     // Placeholder for comments view
+                    // Add your comments view here
                 }
-                
+
+                // Spacer for layout
                 Spacer()
                 Spacer()
-                
+
                 // Button to get directions
                 Button("احصل على الاتجاهات") {
+                    // Placeholder action to get directions
                     // Add action to get directions
                 }
                 .padding()
@@ -132,6 +182,7 @@ struct DetailsView: View {
         .navigationBarTitleDisplayMode(.inline)
     }
 }
+
 struct DetailsView_Previews: PreviewProvider {
     static var previews: some View {
         DetailsView()
