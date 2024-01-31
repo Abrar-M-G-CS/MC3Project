45de9f0f5c98ddd8e65be0a217cf98801cace5d0
elham34 <elham.alwadie@gmail.com>
2024-01-31 12:17:50 +0300

    update

---
+++ MC3Project/Views/Homepage.swift
@@ -0,0 +1,18 @@
+//
+//  Homepage.swift
+//  MC3Project
+//
+//  Created by Elham on 19/07/1445 AH.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homepage()
+}
